feeb1e9bc7876f3397c0beee1ff9d8d0240a93c3
kaziafin <afinkazi786@gmai.com>
2026-06-18 18:05:55 +0530

    Setup Spring Boot backend

---
+++ backend/smart-workspace/src/main/java/smart_workspace/SmartWorkspaceApplication.java
@@ -0,0 +1,13 @@
+package smart_workspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWorkspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartWorkspaceApplication.class, args);
+	}
+
+}
---
+++ backend/smart-workspace/src/test/java/smart_workspace/SmartWorkspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package smart_workspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartWorkspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
